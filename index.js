9d83df0043514fb576bd659c69aeb71fc686d4e0
Nick Garcia <89nickgarcia@gmail.com>
2017-11-18 07:25:32 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string.toLowerCase())
   return "I can't hear you!";
-} else { (string.toUpperCase())
+} if { (string.toUpperCase())
   return "YES INDEED!"
 }
